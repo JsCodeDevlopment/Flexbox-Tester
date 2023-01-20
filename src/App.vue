8135822d679e31eb1199530491fdb8feea40ee80
Jonata Silva <jonatasilva118@gmail.com>
2023-01-19 23:50:49 -0300

    components add

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,24 @@ import FlexItem from "@/components/flex-item.vue";
 </script>
 
 <template>
+	<div class="contentInput">
+		<p>
+			Selecione o componente que deseja fazer as alterações.
+		</p>
+		<select id="pet-select">
+    		<option value="">--Please choose an option--</option>
+    		<option value="dog">Content</option>
+    		<option value="cat">Items</option>
+		</select>
+		<p>
+			Incremente no input a propriedade e a atribuição desejada.
+			OBS: Caso queira alterar alguma propriedade já presente no código. basta digita-lá novamente com a nova atribuição.
+		</p>
+		<input type="text">
+		<div class="cod">
+			Nessa div será exibido as linhas de cód já feitas.
+		</div>
+	</div>
 	<main class="flex">
 		<div class="content">
 			<FlexItem />
@@ -18,18 +36,15 @@ import FlexItem from "@/components/flex-item.vue";
 	justify-content: center;
 	align-items: center;
 }
-
 .content > div {
 	background-color: #1a1725;
 	opacity: 0.9;
 }
-
 .content {
 	display: flex;
 
 	flex-direction: row;
 	align-items: stretch;
-
 	width: 47vw;
 	height: 80vh;
 	gap: 3px;
@@ -38,7 +53,6 @@ import FlexItem from "@/components/flex-item.vue";
 	background-size: 100% 100%;
 	background-repeat: no-repeat;
 }
-
 @media (max-width: 700px) {
 	.content {
 		height: 45vh;
--- src/components/flex-item.vue
+++ src/components/flex-item.vue
@@ -1,6 +1,11 @@
 <template>
-	<div class="item">Item</div>
+	<div class="item">1</div>
+	<div class="item">2</div>
+	<div class="item">3</div>
+	<div class="item">4</div>
+	<div class="item">5</div>
 </template>
+
 
 <style scoped>
 .item {
@@ -9,5 +14,6 @@
 	flex-shrink: 1;
 	flex-basis: auto;
 	align-self: stretch;
+	padding: 3px;
 }
 </style>
